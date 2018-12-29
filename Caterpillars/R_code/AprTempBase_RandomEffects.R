ef76f969293f0be6720162de905fd15930eccc35
KHMacphie <kirsty.macphie@hotmail.co.uk>
2018-12-29 16:20:40 +0000

    Added model with fewer random effects and simulated

--- Caterpillars/R_code/AprTempBase_RandomEffects.R
+++ Caterpillars/R_code/AprTempBase_RandomEffects.R
@@ -69,27 +69,38 @@ all_data$obs<-as.factor(seq(1,length(all_data[,1])))
 ##### MCMCglmm for base temp model with Apr temp and saving random effect variance #####
 
 k<-1000
+prior2<-list(R=list(V=1,nu=0.002),
+             G=list(G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k),
+                    G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k),
+                    G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k)))
+
 prior3<-list(R=list(V=1,nu=0.002),
              G=list(G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k),
                     G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k),
                     G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k),
                     G1=list(V=1,nu=1,aplha.mu=0,alpha.V=k)))
 
+
 ## Added yearsite as a random effect with pr=TRUE also up to 300k iterations and 30k burnin
+#AprBaseModel_RE has all previous REs, AprBaseModel_RE_2 has site day removed to reduce size
 
-AprBaseModel_RE<- MCMCglmm(caterpillars~date*year+Apr*date+I(date^2), random=~site+sitetree+siteday+yearsite, family="poisson", data=all_data, prior=prior3, nitt=300000, burnin=30000, pr=TRUE)
+#AprBaseModel_RE<- MCMCglmm(caterpillars~date*year+Apr*date+I(date^2), random=~site+sitetree+siteday+yearsite, family="poisson", data=all_data, prior=prior3, nitt=300000, burnin=30000, pr=TRUE)
+#AprBaseModel_RE_2<- MCMCglmm(caterpillars~date*year+Apr*date+I(date^2), random=~site+sitetree+yearsite, family="poisson", data=all_data, prior=prior2, nitt=300000, burnin=30000, pr=TRUE)
 #save(AprBaseModel_RE, file = "~/Documents/PhD/R/Caterpillar analysis/Models/AprBaseModel_RE.RData")
-#AprBaseModel_RE <- load("~/Documents/PhD/R/Caterpillar analysis/Models/AprBaseModel_RE.RData")
+#save(AprBaseModel_RE_2, file = "~/Documents/PhD/R/Caterpillar analysis/Models/AprBaseModel_RE_2.RData")
+AprBaseModel_RE <- load("~/Documents/PhD/R/Caterpillar analysis/Models/AprBaseModel_RE.RData")
+AprBaseModel_RE_2 <- load("~/Documents/PhD/R/Caterpillar analysis/Models/AprBaseModel_RE_2.RData")
 
-summary(AprBaseModel_RE)
+summary(AprBaseModel_RE) # DIC: 11394.1 
+summary(AprBaseModel_RE_2) # DIC: 11541.33
 
-# DIC: 11394.1 
 
 plot(AprBaseModel_RE$VCV) #random effects
 #plot(AprBaseModel_RE$Sol) #fixedeffects   too many.. R shut down..
 autocorr(AprBaseModel_RE$Sol) #looking to level of auto correlation in fixed variables
 
 #check if model generates sensible results
+# AprBaseModel_RE
 AprBaseModel_RE.Sim<-simulate(AprBaseModel_RE,nsim=100)
 sum(all_data$caterpillars)
 par(mfcol=c(1,1))
@@ -100,5 +111,15 @@ propzero <- function(x){return(length(which(x==0))/length(x))}
 hist(apply(AprBaseModel_RE.Sim,2,propzero))
 abline(v=propzero(all_data$caterpillars), col="red")
 
+# AprBaseModel_RE_2
+AprBaseModel_RE_2.Sim<-simulate(AprBaseModel_RE_2,nsim=100)
+sum(all_data$caterpillars)
+par(mfcol=c(1,1))
+hist(apply(AprBaseModel_RE_2.Sim,2,sum))
+abline(v=sum(all_data$caterpillars),col=2)
+
+propzero <- function(x){return(length(which(x==0))/length(x))}
+hist(apply(AprBaseModel_RE.Sim,2,propzero))
+abline(v=propzero(all_data$caterpillars), col="red")
 
 
