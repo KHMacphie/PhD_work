26c9c0be382b8e0bba6deb7a5e7b0f9147ac0e13
KHMacphie <kirsty.macphie@hotmail.co.uk>
2019-02-07 13:19:05 +0000

    Tree catergory proportions by site

--- Caterpillars/R_code/HabitatComposition.R
+++ Caterpillars/R_code/HabitatComposition.R
@@ -17,18 +17,6 @@ library(MCMCglmm)
 habitat <- read.csv("~/Dropbox/2018/Habitats_2018_all.csv")
 table(habitat$site) #checking all there- DLW 7+8 missing, not done yet :(
 
-### converting stands and thickets into no. of small trees
-
-St6 <- *0.5 
-St21 <- *1
-# thickets already as small trees
-
-## making a function for the weighting equation
-S <- 40
-M <- 100
-L <- 250
-weighting <- function(x){return(pi*(x/(2*pi))^2)}  ##weighted by cross secitonal area of the trunk (min possible size for each category)
-
 ######## New data frame with stands, thickets and small trees combined as small trees and other deciduous combined
 #converting 6 stands into small trees and combining oth.decid and conifers (combining pine, yew, juniper and conifer)
 
@@ -55,6 +43,7 @@ habitat$syc6smt <- habitat$X6_sycamore*0.5
 habitat$willow6smt <- habitat$X6_willow*0.5
 habitat$oth.decid6smt <- habitat$X6_oth.decid*0.5
 habitat$conifer6smt <- habitat$X6_conifer_all*0.5
+# thickets already as small trees
 
 # combining stands, thickets and small trees
 habitat$Alder_S <- rowSums(habitat[,c("s_alder", "X21_alder", "alder6smt")], na.rm=TRUE)
@@ -66,39 +55,104 @@ habitat$Willow_S <- rowSums(habitat[,c("s_willow", "willow6smt", "X21_willow", "
 habitat$Conifer_S <- rowSums(habitat[,c("X6_conifer_all", "s_conifer_all")], na.rm=TRUE)
 habitat$OthDecid_S <- rowSums(habitat[,c("X6_oth.decid", "X21_oth.decid", "s_oth.decid", "z_oth.decid")], na.rm=TRUE)
 
+##### Use the weighting function to calculate foliage per tree type and size at each NB
+
+## making a function for the weighting equation
+weighting <- function(x){return(pi*(x/(2*pi)^2))}  ##weighted by cross secitonal area of the trunk in cm^2 (min possible size for each category)
+S <- weighting(40)
+M <- weighting(100)
+L <- weighting(250)
 
 Habitat_byNB <- data.frame(Site=habitat$site, 
                            NB=habitat$nestbox, 
-                           Alder_S=habitat$Alder_S,
-                           Alder_M=habitat$m_alder,
-                           Ash_L=habitat$l_ash,
-                           Ash_M=habitat$m_ash,
-                           Ash_S=habitat$Ash_S,
-                           Aspen_M=habitat$m_aspen,
-                           Aspen_S=habitat$s_aspen,
-                           Beech_S=habitat$Beech_S,
-                           Beech_M=habitat$m_beech,
-                           Beech_L=habitat$l_beech,
-                           Birch_S=habitat$Birch_S,
-                           Birch_M=habitat$m_birch,
-                           Birch_L=habitat$l_birch,
-                           Elm_S=habitat$s_elm,
-                           Elm_M=habitat$m_elm,
-                           Elm_L=habitat$l_elm,
-                           Oak_S=habitat$s_oak,
-                           Oak_M=habitat$m_oak,
-                           Oak_L=habitat$l_oak, 
-                           Sycamore_S=habitat$Sycamore_S,
-                           Sycamore_M=habitat$m_sycamore,
-                           Sycamore_L=habitat$l_sycamore,
-                           Willow_S=habitat$Willow_S,
-                           Willow_M=habitat$m_willow,
-                           Willow_L=habitat$l_willow,
-                           Conifer_S=habitat$Conifer_S,
-                           Conifer_M=habitat$m_conifer_all,
-                           Conifer_L=habitat$l_conifer_all,
-                           #OthDecid_S=habitat$OthDecid_S,  not all numbers yet so won't work..
-                           OthDecid_M=habitat$m_oth.decid,
-                           OthDecid_L=habitat$l_oth.decid)
+                           Alder_S=(S*habitat$Alder_S),
+                           Alder_M=(M*habitat$m_alder),
+                           Ash_L=(L*habitat$l_ash),
+                           Ash_M=(M*habitat$m_ash),
+                           Ash_S=(S*habitat$Ash_S),
+                           Aspen_M=(M*habitat$m_aspen),
+                           Aspen_S=(S*habitat$s_aspen),
+                           Beech_S=(S*habitat$Beech_S),
+                           Beech_M=(M*habitat$m_beech),
+                           Beech_L=(L*habitat$l_beech),
+                           Birch_S=(S*habitat$Birch_S),
+                           Birch_M=(M*habitat$m_birch),
+                           Birch_L=(L*habitat$l_birch),
+                           Elm_S=(S*habitat$s_elm),
+                           Elm_M=(M*habitat$m_elm),
+                           Elm_L=(L*habitat$l_elm),
+                           Oak_S=(S*habitat$s_oak),
+                           Oak_M=(M*habitat$m_oak),
+                           Oak_L=(L*habitat$l_oak), 
+                           Sycamore_S=(S*habitat$Sycamore_S),
+                           Sycamore_M=(M*habitat$m_sycamore),
+                           Sycamore_L=(L*habitat$l_sycamore),
+                           Willow_S=(S*habitat$Willow_S),
+                           Willow_M=(M*habitat$m_willow),
+                           Willow_L=(L*habitat$l_willow),
+                           Conifer_S=(S*habitat$Conifer_S),
+                           Conifer_M=(M*habitat$m_conifer_all),
+                           Conifer_L=(L*habitat$l_conifer_all),
+                           #OthDecid_S=(S*habitat$OthDecid_S),  not all numbers yet so won't work..
+                           OthDecid_M=(M*habitat$m_oth.decid),
+                           OthDecid_L=(L*habitat$l_oth.decid))
+Habitat_byNB[is.na(Habitat_byNB)] <- 0
 
-##### Next step is to use the weighting function to calculate foliage per tree type at each NB
+#Foliage score for each tree type at each NB
+Habitat_byNB$Alder_FS <- rowSums(Habitat_byNB[,c("Alder_S", "Alder_M")], na.rm=TRUE)
+Habitat_byNB$Ash_FS <- rowSums(Habitat_byNB[,c("Ash_S", "Ash_M", "Ash_L")], na.rm=TRUE)
+Habitat_byNB$Aspen_FS <- rowSums(Habitat_byNB[,c("Aspen_S", "Aspen_M")], na.rm=TRUE)
+Habitat_byNB$Beech_FS <- rowSums(Habitat_byNB[,c("Beech_S", "Beech_M", "Beech_L")], na.rm=TRUE)
+Habitat_byNB$Birch_FS <- rowSums(Habitat_byNB[,c("Birch_S", "Birch_M", "Birch_L")], na.rm=TRUE)
+Habitat_byNB$Elm_FS <- rowSums(Habitat_byNB[,c("Elm_S", "Elm_M", "Elm_L")], na.rm=TRUE)
+Habitat_byNB$Oak_FS <- rowSums(Habitat_byNB[,c("Oak_S", "Oak_M", "Oak_L")], na.rm=TRUE)
+Habitat_byNB$Sycamore_FS <- rowSums(Habitat_byNB[,c("Sycamore_S", "Sycamore_M", "Sycamore_L")], na.rm=TRUE)
+Habitat_byNB$Willow_FS <- rowSums(Habitat_byNB[,c("Willow_S", "Willow_M", "Willow_L")], na.rm=TRUE)
+Habitat_byNB$Conifer_FS <- rowSums(Habitat_byNB[,c("Conifer_S", "Conifer_M", "Conifer_L")], na.rm=TRUE)
+#Habitat_byNB$OthDecid_FS <- rowSums(Habitat_byNB[,c("OthDecid_S", "OthDecid_M", "OthDecid_L")], na.rm=TRUE)
+
+## combining NB within each site
+site <- read.csv("Dropbox/master_data/site/site_details.csv")
+Habitat_Site <- Habitat_byNB[,33:42]
+Habitat_Site$Site <- Habitat_byNB$Site
+Habitat_Site <- aggregate(.~Site, Habitat_Site, sum)
+
+# gettign proportions fo each tree category
+Habitat_Site$Total <- rowSums(Habitat_Site[2:11])
+Habitat_Site$Alder_prop <- Habitat_Site$Alder_FS/Habitat_Site$Total
+Habitat_Site$Ash_prop <- Habitat_Site$Ash_FS/Habitat_Site$Total
+Habitat_Site$Aspen_prop <- Habitat_Site$Aspen_FS/Habitat_Site$Total
+Habitat_Site$Beech_prop <- Habitat_Site$Beech_FS/Habitat_Site$Total
+Habitat_Site$Birch_prop <- Habitat_Site$Birch_FS/Habitat_Site$Total
+Habitat_Site$Elm_prop <- Habitat_Site$Elm_FS/Habitat_Site$Total
+Habitat_Site$Oak_prop <- Habitat_Site$Oak_FS/Habitat_Site$Total
+Habitat_Site$Sycamore_prop <- Habitat_Site$Sycamore_FS/Habitat_Site$Total
+Habitat_Site$Willow_prop <- Habitat_Site$Willow_FS/Habitat_Site$Total
+Habitat_Site$Conifer_prop <- Habitat_Site$Conifer_FS/Habitat_Site$Total
+#Habitat_Site$OthDecid_prop <- Habitat_Site$OthDecid_FS/Habitat_Site$Total
+
+#checking its correct
+Habitat_Site$propadd <- rowSums(Habitat_Site[,13:22]) # it is
+
+# is it different using mean or sum
+Habitat_Site_mean <- Habitat_byNB[,33:42]
+Habitat_Site_mean$Site <- Habitat_byNB$Site
+Habitat_Site_mean <- aggregate(.~Site, Habitat_Site_mean, mean)
+
+# gettign proportions fo each tree category
+Habitat_Site_mean$Total <- rowSums(Habitat_Site_mean[2:11])
+Habitat_Site_mean$Alder_prop <- Habitat_Site_mean$Alder_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Ash_prop <- Habitat_Site_mean$Ash_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Aspen_prop <- Habitat_Site_mean$Aspen_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Beech_prop <- Habitat_Site_mean$Beech_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Birch_prop <- Habitat_Site_mean$Birch_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Elm_prop <- Habitat_Site_mean$Elm_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Oak_prop <- Habitat_Site_mean$Oak_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Sycamore_prop <- Habitat_Site_mean$Sycamore_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Willow_prop <- Habitat_Site_mean$Willow_FS/Habitat_Site_mean$Total
+Habitat_Site_mean$Conifer_prop <- Habitat_Site_mean$Conifer_FS/Habitat_Site_mean$Total
+#Habitat_Site_mean$OthDecid_prop <- Habitat_Site_mean$OthDecid_FS/Habitat_Site_mean$Total
+
+#checking its correct
+Habitat_Site_mean$propadd <- rowSums(Habitat_Site_mean[,13:22]) # it is
+
